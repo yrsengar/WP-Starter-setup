8254cda65d4bbfad34d0a54c078bce77019f9588
Yogesh Senger <yogesh@strakertranslations.com>
2016-05-27 16:10:51 +1200

    Initial WP core files

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WP_Starter
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-starter' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wp-starter' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-starter' ), 'wp-starter', '<a href="http://www.simplebrackets.com" rel="designer">Yogesh Sengar</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
